ca5b95f031dc29dbc9cb3f52b795f90dd70c5f11
ShaliniSinha35 <shalinisinha3560@gmail.com>
2024-08-03 17:49:10 +0530

    first commit 03.08.24

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ module.exports = {
   port: process.env.PORT  ,
   
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    name: process.env.DB_NAME || 'mahilamediplex',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
     timezone: process.env.DB_TIMEZONE || 'Asia/Kolkata',
   },
   jwtSecret: process.env.JWT_SECRET,
